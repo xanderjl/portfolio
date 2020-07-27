b0e927893f6f7cce8e3568f67fce2f1dff1a9d27
XanderJL <alexanderjameslow@gmail.com>
2020-07-26 23:22:35 -0400

    fixed: footer padding

--- site/src/components/footer.js
+++ site/src/components/footer.js
@@ -3,20 +3,22 @@ import React from "react"
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="content has-text-centered">
-        <div className="level">
-          <div className="leve-left">
-            <a
-              className="is-relative"
-              href="mailto:me@alexlow.dev"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              me@alexlow.dev
-            </a>
+      <div className="container">
+        <div className="content has-text-centered">
+          <div className="level">
+            <div className="leve-left">
+              <a
+                className="is-relative"
+                href="mailto:me@alexlow.dev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                me@alexlow.dev
+              </a>
+            </div>
           </div>
+          <span>&copy; Alex Low 2020</span>
         </div>
-        <span>&copy; Alex Low 2020</span>
       </div>
     </footer>
   )
