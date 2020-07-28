818a0beda15306d968e9f552bbf7dae2a3622a41
XanderJL <alexanderjameslow@gmail.com>
2020-07-28 12:05:55 -0400

    added: data query for projects in CMS

--- site/src/pages/portfolio.js
+++ site/src/pages/portfolio.js
@@ -2,8 +2,70 @@ import React from "react"
 import { graphl } from "gatsby"
 import Layout from "../components/layout"
 
-const portfolio = () => {
-  return <Layout title="Portfolio"></Layout>
+const portfolio = ({ data }) => {
+  const projects = data.projects.edges
+
+  return (
+    <Layout title="Portfolio">
+      {projects.map(({ node: project }) => {
+        const {
+          id,
+          title,
+          slug,
+          repoUrl,
+          projectUrl,
+          image,
+          technologies,
+        } = project
+        return (
+          <div key={id} className="card">
+            
+          </div>
+        )
+      })}
+    </Layout>
+  )
 }
+
+export const data = graphql`
+  {
+    projects: allSanityProjects {
+      edges {
+        node {
+          id
+          title
+          slug {
+            current
+          }
+          repoUrl
+          projectUrl
+          image {
+            asset {
+              fluid(maxWidth: 800, maxHeight: 600) {
+                ...GatsbySanityImageFluid
+              }
+            }
+            hotspot {
+              x
+              y
+            }
+          }
+          technologies {
+            id
+            title
+            icon {
+              asset {
+                fixed(width: 80, height: 80) {
+                  ...GatsbySanityImageFixed
+                }
+              }
+            }
+            url
+          }
+        }
+      }
+    }
+  }
+`
 
 export default portfolio
