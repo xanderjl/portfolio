e457c5563eea389a159dbd49e590c02ef93b0521
XanderJL <alexanderjameslow@gmail.com>
2020-07-29 14:23:06 -0400

    added: uses page for tech stack

---
+++ site/src/pages/uses.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import Layout from "../components/layout"
+
+const uses = () => {
+  return <div></div>
+}
+
+export default uses
