4e985d544032954e4b22c650d93c3df9ea07fbbc
XanderJL <alexanderjameslow@gmail.com>
2020-07-29 17:38:49 -0400

    added: cute illustration placeholder :^ )

--- site/src/pages/blog.js
+++ site/src/pages/blog.js
@@ -1,9 +1,19 @@
 import React from "react"
-import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import svg from "../images/under_construction.svg"
 
 const blog = () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <section className="section">
+        <div className="container">
+          <h1 className="title is-size-1">I'm Working On It!</h1>
+          <h2 className="subtitle is-size-3">I Promise</h2>
+          <img src={svg} alt="construction illustration" />
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 export default blog
