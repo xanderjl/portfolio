9186988348a974ac2394bc9df764d3dc4d791fbf
XanderJL <alexanderjameslow@gmail.com>
2020-07-29 14:23:15 -0400

    added: uses page for tech stack

--- site/src/pages/uses.js
+++ site/src/pages/uses.js
@@ -3,8 +3,20 @@ import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 
-const uses = () => {
-  return <div></div>
+const uses = ({ data }) => {
+  return (
+    <Layout title="Uses">
+      <section className="section">
+        <div className="container"></div>
+      </section>
+    </Layout>
+  )
 }
+
+// export const data = graphql`
+//   {
+
+//   }
+// `
 
 export default uses
