7572df48ba55a8b8acfc8a0fb9e7f1786ee81e36
XanderJL <alexanderjameslow@gmail.com>
2020-07-29 21:06:13 -0400

    added: title for blog page

--- site/src/pages/blog.js
+++ site/src/pages/blog.js
@@ -4,7 +4,7 @@ import svg from "../images/under_construction.svg"
 
 const blog = () => {
   return (
-    <Layout>
+    <Layout title="Blog">
       <section className="section">
         <div className="container">
           <h1 className="title is-size-1">I'm Working On It!</h1>
