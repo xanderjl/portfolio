de5e304ac985c963d799cf0eff89ada8956c2149
XanderJL <alexanderjameslow@gmail.com>
2020-08-02 00:32:29 -0400

    added: animations to blog page

--- site/src/pages/blog.js
+++ site/src/pages/blog.js
@@ -1,16 +1,67 @@
 import React from "react"
+import { motion } from "framer-motion"
+
 import Layout from "../components/layout"
 import svg from "../images/under_construction.svg"
+
+const textVariations = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      type: "tween",
+      when: "beforeChildren",
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+const textChild = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+}
 
 const blog = () => {
   return (
     <Layout title="Blog">
       <section className="section">
-        <div className="container">
-          <h1 className="title is-size-1-desktop">I'm Working On It!</h1>
-          <h2 className="subtitle is-size-3-desktop">I Promise</h2>
-          <img src={svg} alt="construction illustration" />
-        </div>
+        <motion.div
+          className="container"
+          initial="hidden"
+          animate="visible"
+          variants={textVariations}
+        >
+          <motion.h1 className="title is-size-1-desktop" variants={textChild}>
+            I'm Working On It!
+          </motion.h1>
+          <motion.h2
+            className="subtitle is-size-3-desktop"
+            variants={textChild}
+          >
+            I Promise
+          </motion.h2>
+          <motion.img
+            src={svg}
+            alt="construction illustration"
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, type: "spring", stiffness: 100 }}
+          />
+        </motion.div>
       </section>
     </Layout>
   )
