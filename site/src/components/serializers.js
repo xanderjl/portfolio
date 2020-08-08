a2f3f6f90dc1ee2593dfb99b9c120fd5ec257af2
XanderJL <alexanderjameslow@gmail.com>
2020-08-08 14:45:16 -0400

    added: blog post template and serializers for portable text

--- site/src/components/serializers.js
+++ site/src/components/serializers.js
@@ -1,18 +1,53 @@
-import React from "react"
+import React, { createElement } from "react"
 import PortableText from "@sanity/block-content-to-react"
+import urlBuilder from "@sanity/image-url"
 
 export const BlockRenderer = props => {
   const { style = "normal" } = props.node
 
-  if (style === "h1") {
-    return React.createElement(
-      style,
-      { className: `is-size-4-mobile` },
-      props.children
-    )
-  }
+  switch (style) {
+    case "h1":
+      return createElement(
+        style,
+        { className: "title is-size-1  is-size-3-mobile" },
+        props.children
+      )
+      break
 
-  return PortableText.defaultSerializers.types.block(props)
+    case "h2":
+      return createElement(
+        style,
+        { className: "title is-size-3 is-size-5-mobile" },
+        props.children
+      )
+      break
+
+    case "h3":
+      return createElement(
+        style,
+        { className: "title is-size-4 is-size-6-mobile" },
+        props.children
+      )
+      break
+
+    default:
+      return PortableText.defaultSerializers.types.block(props)
+  }
+}
+
+export const BlockImage = ({ node }) => {
+  const { image, alt } = node
+  const urlFor = src =>
+    urlBuilder({
+      projectId: process.env.SANITY_ID,
+      dataset: process.env.SANITY_DATASET,
+    }).image(src)
+
+  return <img src={urlFor(image)} alt={alt} />
+}
+
+export const Code = ({ node }) => {
+  return <pre>{JSON.stringify(node, null, 2)}</pre>
 }
 
 export const LinkTag = ({ mark, children }) => {
--- site/src/templates/blog-post.js
+++ site/src/templates/blog-post.js
@@ -1,11 +1,28 @@
 import React from "react"
 import { graphql } from "gatsby"
+import PortableText from "@sanity/block-content-to-react"
+import { BlockRenderer, BlockImage, Code } from "../components/serializers"
 
 const BlogPost = ({ data }) => {
+  const { title, slug, heroImage, _rawBody } = data.sanityPost
   return (
-    <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <>
+      <section className="section">
+        <h1 className="title is-size-1 is-size-3-mobile">{title}</h1>
+      </section>
+      <section className="section">
+        <PortableText
+          blocks={_rawBody}
+          serializers={{
+            types: {
+              block: BlockRenderer,
+              blockImage: BlockImage,
+              code: Code,
+            },
+          }}
+        />
+      </section>
+    </>
   )
 }
 
