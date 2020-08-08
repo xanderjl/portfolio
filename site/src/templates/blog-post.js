95a285489ab55ef3d223bea934040d270932c31f
XanderJL <alexanderjameslow@gmail.com>
2020-08-08 14:09:16 -0400

    added: gatsby background image for hero images

--- site/src/templates/blog-post.js
+++ site/src/templates/blog-post.js
@@ -1,11 +1,36 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-const BlogPost = () => {
+const BlogPost = ({ data }) => {
   return (
     <div>
-      <h1>Jmstack</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
+
+export const data = graphql`
+  query($slug: String!) {
+    sanityPost(slug: { current: { eq: $slug } }) {
+      id
+      title
+      slug {
+        current
+      }
+      heroImage {
+        asset {
+          fluid(maxWidth: 1920) {
+            ...GatsbySanityImageFluid
+          }
+        }
+        hotspot {
+          x
+          y
+        }
+      }
+      _rawBody
+    }
+  }
+`
 
 export default BlogPost
