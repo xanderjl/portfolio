16ffedcc4332e1006fe5da7d049fa6a02b6c1c51
XanderJL <alexanderjameslow@gmail.com>
2020-08-08 14:00:23 -0400

    added: logic for blog post templating

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve("./src/templates/blog-post.js")
+
+  const response = await graphql(`
+    {
+      blogPosts: allSanityPost {
+        edges {
+          node {
+            id
+            title
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (response.errors) {
+    throw response.errors
+  }
+
+  const { blogPosts } = response.data
+
+  blogPosts.edges.map(({ node: post }) => {
+    const path = `/blog/${post.slug.current}`
+
+    createPage({
+      path,
+      component: blogPostTemplate,
+      context: { slug: post.slug.current },
+    })
+  })
+}
---
+++ site/src/templates/blog-post.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const BlogPost = () => {
+  return (
+    <div>
+      <h1>Jmstack</h1>
+    </div>
+  )
+}
+
+export default BlogPost
