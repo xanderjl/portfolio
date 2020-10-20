cec0f70d9bd46a630c9cef9e67866a4423a46f1a
XanderJL <alexanderjameslow@gmail.com>
2020-10-19 20:03:18 -0400

    fixed: grid styling and other

--- site/src/pages/portfolio.js
+++ site/src/pages/portfolio.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { Link, Heading, Container, Box, Grid, Flex } from "@chakra-ui/core"
+import PortableText from "@sanity/block-content-to-react"
 import Img from "gatsby-image"
 import { motion } from "framer-motion"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { GithubIcon } from "../components/icons"
+import { BlockRenderer, BlogLink } from "../components/serializers"
 
 const portfolio = ({ data }) => {
   const projects = data.projects.edges
@@ -19,9 +21,9 @@ const portfolio = ({ data }) => {
         <Grid
           gridTemplateColumns={{
             base: "minmax(0, 1fr)",
-            md: "repeat(2, 1fr)",
+            md: "repeat(auto-fill, minmax(400px, 1fr))",
           }}
-          gap="2rem"
+          gap="3rem"
         >
           {projects.map(({ node: project }, i) => {
             const {
@@ -31,6 +33,7 @@ const portfolio = ({ data }) => {
               projectUrl,
               image,
               technologies,
+              _rawDescription,
             } = project
 
             const { [technologies.length - 1]: last } = technologies
@@ -40,13 +43,14 @@ const portfolio = ({ data }) => {
                 initial={{ y: 40, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ duration: 0.5, delay: `0.${i}`, type: "tween" }}
+                bg="white"
                 boxShadow="0 0.5rem 1em -0.125em rgba(0,0,0,0.1)"
               >
                 <Link href={projectUrl} _hover={{ opacity: "0.75" }}>
                   <Img fluid={image.asset.fluid} />
                 </Link>
-                <Flex flexDir="column" p="1.25rem" bg="white">
-                  <Heading fontFamily="body">
+                <Flex flexDir="column" p="2rem 1.25rem 5rem 1.25rem">
+                  <Heading pb="1rem" fontFamily="body">
                     <Link
                       href={projectUrl}
                       p="0.25rem 0.5rem"
@@ -54,40 +58,58 @@ const portfolio = ({ data }) => {
                         textDecoration: "none",
                         bg: "blue.100",
                         color: "gray.700",
+                        borderRadius: "4px",
                       }}
                     >
                       {title}
                     </Link>
                   </Heading>
                   <Flex>
-                    <Link
-                      href={repoUrl}
-                      p="0.25rem 0.5rem"
-                      _hover={{
-                        textDecoration: "none",
-                        bg: "blue.100",
-                        color: "gray.700",
-                      }}
-                    >
-                      <GithubIcon />
-                    </Link>
-                    {technologies.map(tech => {
-                      const { id, title, url } = tech
-                      return (
-                        <Link
-                          key={id}
-                          href={url}
-                          p="0.25rem 0.5rem"
-                          _hover={{
-                            textDecoration: "none",
-                            bg: "blue.100",
-                            color: "gray.700",
-                          }}
-                        >
-                          {title === last.title ? title : title}
-                        </Link>
-                      )
-                    })}
+                    <Box>
+                      <Link
+                        href={repoUrl}
+                        p="0.5rem"
+                        _hover={{
+                          textDecoration: "none",
+                          bg: "blue.100",
+                          color: "gray.700",
+                          borderRadius: "4px",
+                        }}
+                      >
+                        <GithubIcon boxSize={{ base: 8, md: 12 }} />
+                      </Link>
+                    </Box>
+                    <Flex pb="1rem" wrap="wrap">
+                      {technologies.map(tech => {
+                        const { id, title, url } = tech
+                        return (
+                          <Link
+                            key={id}
+                            href={url}
+                            p="0.25rem 0.5rem"
+                            _hover={{
+                              textDecoration: "none",
+                              bg: "blue.100",
+                              color: "gray.700",
+                              borderRadius: "4px",
+                            }}
+                          >
+                            {title === last.title ? title : title}
+                          </Link>
+                        )
+                      })}
+                    </Flex>
+                  </Flex>
+                  <Flex>
+                    {_rawDescription && (
+                      <PortableText
+                        blocks={_rawDescription}
+                        serializers={{
+                          types: { block: BlockRenderer },
+                          marks: { link: BlogLink },
+                        }}
+                      />
+                    )}
                   </Flex>
                 </Flex>
               </MotionBox>
@@ -108,6 +130,7 @@ export const data = graphql`
           title
           repoUrl
           projectUrl
+          _rawDescription
           image {
             asset {
               fluid(maxWidth: 800, maxHeight: 600) {
