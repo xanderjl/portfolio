b38165c3b4e7465575e7dc0515277367d9008381
Alex Low <me@alexlow.dev>
2021-02-09 05:17:27 -0500

    added: next head fields

--- site/components/layout/index.js
+++ site/components/layout/index.js
@@ -8,7 +8,6 @@ const Layout = ({ title, metadescription, children }) => {
   return (
     <>
       <Head>
-        <html lang="en" />
         <title>{title && `${title} | `}Alex Low</title>
         <meta
           name="description"
@@ -18,6 +17,21 @@ const Layout = ({ title, metadescription, children }) => {
               : "Alex Low is a front end web developer focusing on the Jamstack, based in London, ON."
           }
         />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta
+          property="og:description"
+          content={
+            metadescription
+              ? metadescription
+              : "Alex Low is a front end web developer focusing on the Jamstack, based in London, ON."
+          }
+        />
+        <meta property="og:site_name" content={siteTitle} />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:creator" content={config.social.twitter} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Flex direction="column" minH="100vh" overflowX="hidden" bg="transparent">
