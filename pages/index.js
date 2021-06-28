83238b15cd1bf044f2e817cd54490ba91d7a4377
Xander Low <alexanderjameslow@gmail.com>
2021-06-27 23:03:04 -0400

    added: orbiting particles to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { Suspense, useRef } from "react"
 import { Box, Heading, useTheme } from "@chakra-ui/react"
 import Layout from "@components/layout"
 import { Canvas, useFrame } from "@react-three/fiber"
@@ -8,17 +8,19 @@ import {
   Sphere,
   Stars,
   Torus,
+  useTexture,
 } from "@react-three/drei"
 
 const Lighting = () => (
   <>
     <ambientLight intensity={0.3} />
-    <directionalLight castShadow position={[-5, 10, 10]} />
+    <directionalLight castShadow position={[-5, 10, 10]} intensity={0.8} />
   </>
 )
 
 const DodecahedronFrame = ({ theme }) => {
   const ref = useRef()
+
   useFrame(() => {
     ref.current.rotation.x = ref.current.rotation.y += 0.005
   }, [])
@@ -36,6 +38,7 @@ const DodecahedronFrame = ({ theme }) => {
 
 const IcosahedronFrame = ({ theme }) => {
   const ref = useRef()
+
   useFrame(() => {
     ref.current.rotation.y = ref.current.rotation.x += 0.01
   }, [])
@@ -53,8 +56,9 @@ const IcosahedronFrame = ({ theme }) => {
 
 const Donut = ({ theme }) => {
   const ref = useRef()
+
   useFrame(() => {
-    ref.current.rotation.x = ref.current.rotation.y += 0.005
+    ref.current.rotation.x = ref.current.rotation.y += 0.002
   }, [])
 
   return (
@@ -68,11 +72,48 @@ const Donut = ({ theme }) => {
   )
 }
 
+const OrbitDots = ({ theme }) => {
+  const ref = useRef()
+
+  useFrame(() => {
+    ref.current.rotation.x = ref.current.rotation.y += 0.0005
+  }, [])
+
+  return (
+    <points ref={ref}>
+      <sphereGeometry attach="geometry" args={[80, 10, 6]} />
+      <pointsMaterial
+        attach="material"
+        color={theme.colors.gray[700]}
+        size={0.6}
+      />
+    </points>
+  )
+}
+
+const OrbitX = () => {
+  const ref = useRef()
+  const texture = useTexture("/textures/x.png")
+
+  useFrame(() => {
+    ref.current.rotation.x = ref.current.rotation.y -= 0.001
+  }, [])
+
+  return (
+    <points ref={ref}>
+      <sphereGeometry attach="geometry" args={[30, 4, 4]} />
+      <pointsMaterial attach="material" map={texture} transparent size={0.8} />
+    </points>
+  )
+}
+
 const Orbit = () => {
   const ref = useRef()
+
   useFrame(() => {
     ref.current.rotation.y = ref.current.rotation.y += 0.001
   }, [])
+
   return (
     <Sphere ref={ref} args={[100, 18, 14]}>
       <meshBasicMaterial wireframe />
@@ -102,6 +143,10 @@ const IndexPage = () => {
           <Donut theme={theme} />
           <Stars />
           <Orbit />
+          <OrbitDots theme={theme} />
+          <Suspense fallback={null}>
+            <OrbitX />
+          </Suspense>
         </Canvas>
       </Box>
     </Layout>
