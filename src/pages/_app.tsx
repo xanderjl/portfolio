17e88f4594cf59a4f7e6edc91fc3e9ecf14da846
Xander Low <alexanderjameslow@gmail.com>
2021-12-26 16:27:28 -0800

    added: default seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <DefaultSeo
+        title='Xander Low'
+        description='Xander is a front-end web developer focusing on implementing e-commerce solutions for small businesses on the Jamstack. He has been spending his spare time learning threejs to create unique experiences on the web.'
+        openGraph={{
+          type: 'website',
+          locale: 'en_CA',
+          title: 'Xander Low',
+          description:
+            'Xander is a front-end web developer focusing on implementing e-commerce solutions for small businesses on the Jamstack. He has been spending his spare time learning threejs to create unique experiences on the web.',
+          url: 'https://www.xandydandy.com',
+          site_name: 'Xander Low'
+        }}
+        twitter={{
+          handle: '@XandyDandy',
+          cardType: 'summary_large_image'
+        }}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   )
